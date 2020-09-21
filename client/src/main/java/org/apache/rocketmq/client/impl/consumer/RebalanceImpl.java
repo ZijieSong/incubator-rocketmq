0f3afca9dc5743ab35dca4acb79cc2ed6185c4fa
mac-szj <393593720@qq.com>
2020-09-21 23:59:20 +0800

    update

--- client/src/main/java/org/apache/rocketmq/client/impl/consumer/RebalanceImpl.java
+++ client/src/main/java/org/apache/rocketmq/client/impl/consumer/RebalanceImpl.java
@@ -418,11 +418,18 @@ public abstract class RebalanceImpl {
         List<PullRequest> pullRequestList = new ArrayList<>(); // 拉消息请求数组
         for (MessageQueue mq : mqSet) {
             if (!this.processQueueTable.containsKey(mq)) {
+
                 //这里分布式锁的意义在于，考虑这么一种情况
                 //a,b,c三个队列，1，2两个消费者，a由1消费，b，c由2消费
-                //一个订单的新增和删除发到c队列中，2先拿到新增的消息进行处理，假设处理很久，卡在这里了
+                //假设A B C D四个消息通过c队列发给2号消费者已经处理完毕
+                //但此时只是保存了A B这两条消息的进度，C D仍只在本地内存，但没有发给broker
                 //这时新上线3号消费者，rebalance，欲将c分配给3号
-                //假如没有全局锁，那么直接分配成功，那么
+                //假如没有全局锁，那么直接分配成功，那么3号在消费时会重新拉取到C D这两条消息
+                //整体来看就是按照A B C D C D这个顺序来消费，不符合顺序消费原则
+                //如果有了全局锁，那么在移除2号消费者对队列c的监听时，会去persist进度到broker，也就是 C D的进度会同步broker
+                //然后2号消费者才会释放掉全局锁，3号去尝试加锁，当加锁成功后去获取最新的消费进度
+                //那么新上线的3号消费者拿到的就是D+1的进度，不会重复也就是乱序消费了
+
                 if (isOrder && !this.lock(mq)) { // 顺序消息锁定消息队列
                     log.warn("doRebalance, {}, add a new mq failed, {}, because lock failed", consumerGroup, mq);
                     continue;
